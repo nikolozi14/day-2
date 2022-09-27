a74c13efc73a8dfc7e1bba1a18de594aafa9b19a
nikolozi14 <nikolozchichikashvili59@gmail.com>
2022-09-27 11:22:46 -0700

    day2 commit

---
+++ lesson2.py
@@ -0,0 +1,23 @@
+name="nika"
+surname="chichikashvili"
+hobby="gym"
+print(len(name))
+print(len(surname))
+print(len(hobby))
+
+
+my_sentence = """გამარჯობა,
+მე ვარ ნიკა,
+თოთხმეტი წლის
+და ვსწავლობ პროგრამირებას."""
+
+
+
+if 10>5:
+    print(name)
+
+if 5>2:
+    print(surname)
+
+if 10-5==5:
+    print(hobby)
